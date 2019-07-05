719b8de01f55ee382d3bed32d61b1ba9dec13f9e
Satyajit Dalai <satyajitdalai96@gmail.com>
2019-07-05 08:10:21 +0530

    fixed menu inline and new user added

--- Admin/connectheader.php
+++ Admin/connectheader.php
@@ -7,15 +7,19 @@
 	<link rel="stylesheet" type="text/css" href="html/css/cust.css">
 </head>
 <body>
-	<div class="container-fluid header">
+	<div class="container header">
 		<div class="row">
-		<div class="col-sm-6">
+		<div class="col-sm-2">
+			LOGO HERE
+		</div>
+		<div class="col">
 			<ul class="list-inline">
 				<li><a href="index.php">Home</a></li>
-				<li><a href="listblog.php">bloglist</a></ul>
+				<li><a href="listblog.php">bloglist</a></li>
 				<li><a href="bloginsert.php">bloginsert</a></li>
 				<li><a href="#">contact us</a></li>
 				<li><a href="list.php">User</a></li>
+				<li><a href="insert.php">New User</a></li>
 				<li><a href="logout.php">logout</a></li>
 			</ul>
 		</div>
