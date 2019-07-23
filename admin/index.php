baef730a644b485d9ebb478d99ca125cdafa5715
Satyajit Dalai <satyajitdalai96@gmail.com>
2019-07-23 15:13:12 +0530

    modified  file indentation and error fix changes

--- admin/index.php
+++ admin/index.php
@@ -2,6 +2,8 @@
  	include 'includes/connectheader.php';
 
 	if(!isset($_SESSION['first_name'])){
+		$fname = $_SESSION['fname'];
+		$lname = $_SESSION['lname'];
 		header('location:login.php');
 	}
 	?>
@@ -9,11 +11,10 @@
 		<h1 align="center">
 			<?php
 				//session_start();
-				//print_r($_SESSION);
-		echo "Welcome"." To Inventory Management System ".''.'</br>';
-		echo $_SESSION['first_name'].' '.$_SESSION['last_name'];
-		?>
-			
+				// print_r($_SESSION);
+			echo "Welcome"." To Inventory Management System ".''.'</br>';
+			echo $fname.' '.$lname;
+				?>	
 		</h2>
 	</div>
 
