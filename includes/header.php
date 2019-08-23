0a01db9ffd492397719341db1171de8e51211605
Satyajit Dalai <satyajitdalai96@gmail.com>
2019-08-23 13:53:43 +0530

    frontend file integreat with backend connection includes

---
+++ includes/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<!-- <meta name="viewport" content="width=device-width,initial-scale=1"> -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap4.3.min.css">
+	<
+	<link rel="stylesheet" type="text/css" href="css/front.css">
+	<!-- <script type="text/javascript" src="js/bootstrap.bundele.min.js"></script> -->
+	<title></title>
+	<style type="text/css">
+
+	</style>
+	
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark fixed-top">
+	<div class="container">
+	    <a class="navbar-brand" href="#">Start Bootstrap</a>
+	    <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarResponsive" arial-controls="navbarResponsive" aria-expanded="false" arial-label="toggler navigation">
+   			<span class="navbar-toggler-icon"></span>
+  		</button>
+
+ 		<div class="navbar-collapse collapse show" id="navbarResponsive">
+
+  			<ul class="navbar-nav ml-auto">
+				<li class="nav-item active">
+      				<a class="nav-link" href="#">Home</a>
+    			</li>
+    			<li class="nav-item">
+      				<a class="nav-link" href="#">About</a>
+    			</li>
+				<li class="nav-item">
+      				<a class="nav-link" href="#">Services</a>
+    			</li>
+    			<li class="nav-item">
+      				<a class="nav-link" href="#">Contact</a>
+    			</li>
+			</ul>
+		</div>
+	</div>
+</nav>
